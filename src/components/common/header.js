c8e338297fd4c2bd588d85bb9fad01e8c761cf3a
Trần Phúc Thành <enigma15012001@gmail.com>
2021-11-11 08:28:51 +0700

    done header

---
+++ src/components/common/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Menu } from 'antd';
+const AppHeader = () => {
+  return (
+    <div className="container-fluid">
+      <div className="header">
+        <div className="logo">
+          <i class="fas fa-bolt"></i>
+          <a href="google.com">Tech</a>
+        </div>
+        <Menu mode="horizontal" defaultSelectedKeys={['home']}>
+          <Menu.Item key="home">home</Menu.Item>
+          <Menu.Item key="about">about</Menu.Item>
+          <Menu.Item key="features">features</Menu.Item>
+          <Menu.Item key="howitworks">How it works</Menu.Item>
+          <Menu.Item key="faq">FAQ</Menu.Item>
+          <Menu.Item key="pricing">pricing</Menu.Item>
+          <Menu.Item key="contact">contact</Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  );
+};
+
+export default AppHeader;
